128c51eedf33ae0716224bacdb220532ad857521
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-03-03 10:41:18 +0100

    Add method to extract domain relevance for each term

--- terminology_extraction.py
+++ terminology_extraction.py
@@ -2,6 +2,8 @@
 """
 Preprocess Terminology extraction
 """
+from nltk.corpus import reuters
+
 from candidates import Candidates
 
 class Terminology:
@@ -11,11 +13,35 @@ class Terminology:
         self.reference = reference
         self.candidates = candidates
         self.domain_relevance = dict()
+        self.r = Candidates(self.reference, from_dir=False)
 
     def _domain_relevance(self):
-        d = Candidates(self.domain)
-        r = Candidates(self.reference, from_file=False)
-        d_sum = d.sum_unigrams
-        r_sum = r.sum_unigrams
-        for canidate in self.candidates:
-            pass
+        s = self._count_reference()
+        for candidate in self.candidates:
+            if s == 0:
+                p = 0
+            else:
+                p = self.r.bigrams.get(candidate, 0) / s
+            term_relevance = self.candidates[candidate] / (self.candidates[candidate]
+                                                          + p)
+            self.domain_relevance[candidate] = term_relevance
+
+    def _count_reference(self):
+        s = 0
+        for c in self.candidates:
+            s += self.r.bigrams.get(c, 0)
+        return s
+
+    def _domain_consens(self):
+        pass
+
+if __name__ == "__main__":
+    can = dict()
+    with open("cand.txt") as file:
+        for line in file:
+            line = line.rstrip().split("\t")
+            words = tuple(line[0].split())
+            can[words] = int(line[1])
+    t = Terminology("acl_texts", reuters.words(), can)
+    print("Done")
+    t._domain_relevance()
