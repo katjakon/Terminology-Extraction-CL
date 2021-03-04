2dd924318ca2f1c9ffa2e180abd9af7416a290b2
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-03-04 13:32:28 +0100

    Use PlainTextCorpusReader when handling corpus

--- candidates.py
+++ candidates.py
@@ -10,72 +10,70 @@ from nltk import bigrams
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 from nltk.tokenize import WordPunctTokenizer
+from nltk.corpus.reader.plaintext import PlaintextCorpusReader
 
 
-class Candidates:
+class Preprocess:
 
     PUNC = set(string.punctuation)
 
-    def __init__(self, corpus, stops=None, from_dir=True):
-        if stops is None:
-            stops = []
+    def __init__(self, corpus, from_dir=True):
+        if from_dir:
+            corpus = PlaintextCorpusReader(corpus, ".*")
         self.corpus = corpus
-        self.stops = stops
         self.bigrams = FreqDist()
-        self.unigrams = FreqDist()
-        self.from_dir = from_dir
         self._count()
 
     @property
-    def sum_unigrams(self):
-        count = 0
-        for token in self.unigrams:
-            count += self.unigrams[token]
-        return count
-
-    @property
     def sum_bigrams(self):
-        count = 0
-        for bigram in self.bigrams:
-            count += self.bigrams[bigram]
-        return count
+        return sum(self.bigrams[bigram] for bigram in self.bigrams)
 
     def _count(self):
-        if self.from_dir:
-            txt = [file for file in os.listdir(self.corpus) if file.endswith(".txt")]
-            for file in txt:
-                with open(os.path.join(self.corpus, file), encoding="utf-8") as file:
-                    for line in file:
-                        line = WordPunctTokenizer().tokenize(line.rstrip().lower())
-                        self.count_bigrams(bigrams(line))
-                        self.count_unigrams(line)
-        else:
-            self.count_bigrams(bigrams(self.corpus))
-            self.count_unigrams(self.corpus)
-
-    def count_bigrams(self, bigram_list):
-        for bigram in bigram_list:
+        words = [word.lower() for word in self.corpus.words()]
+        bigrams_words = bigrams(words)
+        for bigram in bigrams_words:
             self.bigrams[bigram] += 1
 
-    def count_unigrams(self, uni_list):
-        for unigram in uni_list:
-            self.unigrams[unigram] += 1
-
-    def get_candidates(self, prop=-15):
+    def get_candidates(self, prop=-15, stops=None):
+        if stops is None:
+            stops = []
         candidates = dict()
         sum_bigrams = self.sum_bigrams
         for word_i, word_j in self.bigrams:
-            if word_i not in self.stops and word_j not in self.stops:
+            if word_i not in stops and word_j not in stops:
                 if word_i not in self.PUNC and word_j not in self.PUNC:
                     log_bi = math.log(self.bigrams[(word_i, word_j)]/sum_bigrams)
                     if log_bi >= prop:
                         candidates[(word_i, word_j)] = self.bigrams[(word_i, word_j)]
         return candidates
 
+    def bigram_freq(self, bigram_list):
+        return {bigram: self.bigrams.get(bigram, 0) for bigram in bigram_list}
+
+    def bigrams_to_file(self, bigrams, file_dir="preprocess", name="candidates", ext=".txt"):
+        if not os.path.isdir(file_dir):
+            os.mkdir(file_dir)
+        in_dir = [file for file in os.listdir(file_dir)]
+        count = 1
+        file_name = "{}{}{}".format(name, count, ext)
+        while file_name in in_dir:
+            count += 1
+            file_name = "{}{}{}".format(name, count, ext)
+        bi_freq = self.bigram_freq(bigrams)
+        with open(os.path.join(file_dir, file_name), "w", encoding="utf-8") as file:
+            for word_i, word_j in bigrams:
+                file.write("{} {}\t{}\n".format(word_i,
+                                                word_j,
+                                                bi_freq[(word_i, word_j)]))
+
+    def bigrams_from_file(self, fileid):
+        assert fileid in self.corpus.fileids(), "File needs to be in instance corpus."
+        file_words = [word.lower() for word in self.corpus.words(fileid)]
+        bigrams_file = bigrams(file_words)
+        return FreqDist(bigrams_file)
+
+
 
 if __name__ == "__main__":
-    c = Candidates("acl_texts", stops=stopwords.words("english"))
-    d = c.get_candidates()
-    with open("cand.txt", "w") as file:
-        for i, j in d:
-            file.write("{} {}\t{}\n".format(i, j, d[(i, j)]))
+    d = Preprocess("acl_texts/")
+    print(dict(d.bigrams_from_file("text_paper_1.txt")))
