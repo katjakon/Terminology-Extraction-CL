786733e6d7e975ef0b7c7cde4471f21910b0c596
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-03-10 15:13:09 +0100

    Add documentation and clean code

--- preprocess.py
+++ preprocess.py
@@ -1,85 +1,166 @@
 # -*- coding: utf-8 -*-
+# Katja Konermann
+# 802658
 """
-Kandidatenauswahl
+Choose candidates for terminolgy extraction
+and do some preprocessing.
 """
-import os
-import string
 import math
 
 from nltk import bigrams
-from nltk.corpus import stopwords, reuters
 from nltk.corpus.reader.plaintext import PlaintextCorpusReader
 from nltk.probability import FreqDist
 
 
 class Preprocess:
 
-    PUNC = set(string.punctuation)
+    """
+    A class that does some processing of a corpus. A corpus can be
+    an nltk corpus or directory of text files.
 
-    def __init__(self, corpus, from_dir=True):
+    Attributes:
+        corpus
+
+    Methods:
+        is_lexical(word_i, word_j):
+            Check if both words are alpha-numerical.
+        candidates(min_prob=-15, stops=None):
+            Get set of possible bigrams for terminology extraction.
+        get_frequency(bigram_list, fileid=None):
+            Get frequency of bigrams in bigram list in corpus or file.
+        bigrams(fileid=None):
+            Bigrams with frequency in whole corpus or file.
+    """
+
+    def __init__(self, corpus):
+        """
+        Constructs a preprocess instance.
+
+        Args:
+            corpus:
+                Should either be the name of a directory with text files
+                or a nltk corpus.
+
+        Returns:
+            None.
+        """
         if isinstance(corpus, str):
             corpus = PlaintextCorpusReader(corpus, ".*")
         self.corpus = corpus
-        self.bigrams = FreqDist()
+        self._bigrams = FreqDist()
         self._count()
 
     @property
     def sum_bigrams(self):
-        return sum(self.bigrams[bigram] for bigram in self.bigrams)
+        """Sum of frequency of all bigrams in corpus."""
+        freq_bigram = self.bigrams()
+        return sum(freq_bigram[bigram] for bigram in freq_bigram)
 
     def _count(self):
+        """Counts occurences of bigrams in corpus, case insensitive.
+
+        Returns:
+            None.
+        """
+        print("Processing corpus...")
         words = [word.lower() for word in self.corpus.words()]
         bigrams_words = bigrams(words)
         for bigram in bigrams_words:
-            self.bigrams[bigram] += 1
+            self._bigrams[bigram] += 1
 
-    def get_candidates(self, prop=-15, stops=None):
+    @staticmethod
+    def is_lexical(word_i, word_j):
+        """Checks if two words only contain alpha-numeric characters.
+
+        Args:
+            word_i (str): A string to be checked.
+            word_j (str): A string to be checked.
+
+        Returns:
+            bool:
+                True if both token are alpha-numeric,
+                False otherwise.
+        """
+        if word_i.isalnum() and word_j.isalnum():
+            return True
+        return False
+
+    def candidates(self, min_prob=-15.25, stops=None):
+        """
+        Generate a list of possible candidates for terminology extraction.
+
+        A bigram is considered a candidate if it exceeds a minimum proportion
+        of whole corpus, only contains alpha-numeric characters and doesn't
+        contain tokens in stopword list.
+
+        Args:
+            min_prob (float):
+                Minimum probablity a bigram has to have to be considered a
+                candidate. Log probablities are used.
+            stops (list):
+                List of strings. If a bigram contains a word of that list, it
+                is not considered a candidate. If default is used, an empty list
+                is used. Default is None.
+
+        Returns:
+            set:
+                set of tuples containing two strings.
+        """
         if stops is None:
             stops = []
         candidates = set()
         sum_bigrams = self.sum_bigrams
-        for word_i, word_j in self.bigrams:
+        for word_i, word_j in self.bigrams():
             if word_i not in stops and word_j not in stops:
-                if word_i not in self.PUNC and word_j not in self.PUNC:
-                    log_bi = math.log(self.bigrams[(word_i, word_j)]/sum_bigrams)
-                    if log_bi >= prop:
+                if self.is_lexical(word_i, word_j):
+                    prob_bi = math.log(self.bigrams()[word_i, word_j]
+                                       / sum_bigrams)
+                    if prob_bi >= min_prob:
                         candidates.add((word_i, word_j))
         return candidates
 
-    def get_freq(self, bigram_list, fileid=None):
+    def get_frequency(self, bigram_list, fileid=None):
+        """Get the frequency of a list of bigrams
+
+        Either get frequency for the whole corpus or for
+        a specific file. Bigrams that don't occur in corpus/file
+        are not keys in returned dictionaries.
+
+        Args:
+            bigram_list (list):
+                List with two-tuples of strings
+            fileid (str):
+                Id of file in corpus. If default is used, gets frequency
+                in whole corpus. Default is None.
+
+        Returns:
+            dict:
+                Keys are tuples of strings, values are frequencies in
+                file/corpus (int)
+        """
+        freq = self.bigrams(fileid)
+        return {bigr: freq[bigr] for bigr in bigram_list if bigr in freq}
+
+    def bigrams(self, fileid=None):
+        """Frequency of bigrams in file or corpus.
+
+        Args:
+            fileid (str):
+                Id of file in corpus. If default is used, returns bigrams
+                in whole corpus. Default is None.
+
+        Returns:
+            FreqDist:
+                two-tuples of strings are keys, frequency in corpus/file are
+                values.
+        """
         if fileid is not None:
-            freq_f = self.bigrams_in_file(fileid)
-            return {bigram: freq_f[bigram] for bigram in freq_f if bigram in bigram_list}
-        return {bigram: self.bigrams.get(bigram, 0) for bigram in bigram_list}
-
-    def bigrams_to_file(self,
-                        bigrams,
-                        file_dir="preprocess",
-                        name="candidates",
-                        ext=".txt"):
-        if not os.path.isdir(file_dir):
-            os.mkdir(file_dir)
-        in_dir = [file for file in os.listdir(file_dir)]
-        count = 1
-        file_name = "{}{}{}".format(name, count, ext)
-        while file_name in in_dir:
-            count += 1
-            file_name = "{}{}{}".format(name, count, ext)
-        bi_freq = self.get_freq(bigrams)
-        with open(os.path.join(file_dir, file_name), "w", encoding="utf-8") as file:
-            for word_i, word_j in bigrams:
-                file.write("{} {}\t{}\n".format(word_i,
-                                                word_j,
-                                                bi_freq[(word_i, word_j)]))
-
-    def bigrams_in_file(self, fileid):
-        assert fileid in self.corpus.fileids(), "File needs to be in corpus."
-        file_words = [word.lower() for word in self.corpus.words(fileid)]
-        bigrams_file = bigrams(file_words)
-        return FreqDist(bigrams_file)
-
+            assert fileid in self.corpus.fileids(), "File not in corpus."
+            file_words = [word.lower() for word in self.corpus.words(fileid)]
+            bigrams_file = bigrams(file_words)
+            return FreqDist(bigrams_file)
+        return self._bigrams
 
 
 if __name__ == "__main__":
-    d = Preprocess("acl_texts")
-    can = d.get_candidates(stops=stopwords.words("english"))
+    pass
